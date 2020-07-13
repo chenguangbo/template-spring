67d0ebaaa17ac0d8e6766f6a915ef800e34f7e41
CGB <cgb2499404424@163.com>
2020-07-13 22:58:10 +0800

    生产者消费者案例   解决demo2中程序不能正常结束的问题  -- 去掉else

---
+++ src/test/java/juc/productorAndConsumer/demo3/TestProductAndConsumer.java
@@ -0,0 +1,94 @@
+package juc.productorAndConsumer.demo3;
+
+public class TestProductAndConsumer {
+
+    //  解决demo2中程序不能正常结束的问题  -- 去掉else
+    public static void main(String[] args) {
+
+        Clerk clerk = new Clerk();
+
+        Productor productor = new Productor(clerk);
+        Consumer consumer = new Consumer(clerk);
+        new Thread(productor, "生产者 A").start();
+        new Thread(consumer, "消费者 B").start();
+    }
+
+
+}
+
+
+class Clerk {
+
+    private int product = 0;
+
+    public synchronized void get() {
+
+        if (product >= 1) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("产品已满");
+        }
+        System.out.println(Thread.currentThread().getName() + " : " + ++product);
+        this.notifyAll();
+
+    }
+
+    public synchronized void sale() {
+        if (product <= 0) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("缺货!");
+        }
+        System.out.println(Thread.currentThread().getName() + " : " + product--);
+        this.notifyAll();
+
+    }
+
+}
+
+// 消费者
+class Consumer implements Runnable {
+    private Clerk clerk;
+
+    public Consumer(Clerk clerk) {
+        this.clerk = clerk;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            clerk.sale();
+        }
+    }
+}
+
+// 生产者
+class Productor implements Runnable {
+    private Clerk clerk;
+
+    public Productor(Clerk clerk) {
+        this.clerk = clerk;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 20; i++) {
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            clerk.get();
+        }
+    }
+}
+
+
+
+
