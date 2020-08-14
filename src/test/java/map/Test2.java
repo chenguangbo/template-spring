50f2e6922716fad31acfba51eb95031bb26ed806
CGB <cgb2499404424@163.com>
2020-08-14 23:10:30 +0800

    Thread yield

--- src/test/java/map/Test2.java
+++ src/test/java/map/Test2.java
@@ -1,12 +1,17 @@
 package map;
 
 import java.util.HashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 
 public class Test2 {
     public static void main(String[] args) {
         HashMap hashMap = new HashMap();
         hashMap.put(null,1);
-
+        new ScheduledThreadPoolExecutor(10);
+        ScheduledExecutorService scheduledExecutorService = Executors.newScheduledThreadPool(8);
+        boo
     }
 }
